6176715bfa7a0a5dbce7642a31c388413d1618cc
GitHub Action <action@github.com>
2021-11-08 09:01:12 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sganeshkanna/templatesample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sganeshkanna.templatesample.services
+
+import com.github.sganeshkanna.templatesample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
